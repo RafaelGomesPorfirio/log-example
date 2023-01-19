0b46cc8156ae6b732e928ab572692886155ea1d2
Rafael <rafagomes475@gmail.com>
2023-01-18 21:56:56 -0300

    atl

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,5 +47,21 @@ class HomeController extends Controller
 
         return $sub;
     }
+
+    public function div($num1, $num2) {
+        if ($num2 == 0) {
+            logger()->error('Divisor zero!');
+
+            abort(404);
+        } else {
+            $div = $num1 / $num2;
+
+            logger()->info('Div feita');
+
+            return $div;
+        }
+
+    }   
+    
 }
  
